f1bc64bfd6eec34bc547d6656e5a354a257ee67d
Uvaish Zafri <35339873+uvaishzafri@users.noreply.github.com>
2019-02-24 00:51:55 +0530

    basic fixes

--- app/src/main/java/com/example/myapplication/contacts.java
+++ app/src/main/java/com/example/myapplication/contacts.java
@@ -3,7 +3,8 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class contacts extends AppCompatActivity {
+public class
+contacts extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
